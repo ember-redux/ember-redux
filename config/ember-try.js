c6fe1678e0b1413bbe78e953361aa9c87b6a4fb6
toran billups <toranb@gmail.com>
2016-03-25 19:55:04 -0500

    [BUILD]: updated travis to only run ember 2.3/2.4

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,12 +2,6 @@
 module.exports = {
   scenarios: [
     {
-      name: 'ember-1.13.X',
-      dependencies: {
-        'ember': '~1.13.0'
-      }
-    },
-    {
       name: 'ember-2.3.X',
       dependencies: {
         'ember': '~2.3.0'
