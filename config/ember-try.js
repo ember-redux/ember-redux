a1aac49f0431e4d4375423085fbcf61eceeaf595
toran billups <toranb@gmail.com>
2016-03-25 19:45:30 -0500

    [BUILD]: more ember-try config work

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,53 +2,21 @@
 module.exports = {
   scenarios: [
     {
-      name: 'default',
-      bower: {
-        dependencies: { }
+      name: 'ember-1.13.X',
+      dependencies: {
+        'ember': '~1.13.0'
       }
     },
     {
-      name: 'ember-1-13',
-      bower: {
-        dependencies: {
-          'ember': '~1.13.0'
-        },
-        resolutions: {
-          'ember': '~1.13.0'
-        }
+      name: 'ember-2.3.X',
+      dependencies: {
+        'ember': '~2.3.0'
       }
     },
     {
-      name: 'ember-release',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#release'
-        },
-        resolutions: {
-          'ember': 'release'
-        }
-      }
-    },
-    {
-      name: 'ember-beta',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#beta'
-        },
-        resolutions: {
-          'ember': 'beta'
-        }
-      }
-    },
-    {
-      name: 'ember-canary',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#canary'
-        },
-        resolutions: {
-          'ember': 'canary'
-        }
+      name: 'ember-2.4.X',
+      dependencies: {
+        'ember': '~2.4.0'
       }
     }
   ]
