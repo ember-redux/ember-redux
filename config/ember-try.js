a1e6ffab43798e6db1c1b4ad9a1518d8eee47d4c
toran billups <toranb@gmail.com>
2016-07-08 00:15:18 -0500

    [BUILD]: added ember-beta to ember-try

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,15 +2,21 @@
 module.exports = {
   scenarios: [
     {
-      name: 'ember-2.3.X',
+      name: 'ember-2.5.X',
       dependencies: {
-        'ember': '~2.3.0'
+        'ember': '~2.5.0'
       }
     },
     {
-      name: 'ember-2.4.X',
+      name: 'ember-2.6.X',
       dependencies: {
-        'ember': '~2.4.0'
+        'ember': '~2.6.0'
+      }
+    },
+    {
+      name: 'ember-beta',
+      dependencies: {
+        'ember': 'components/ember#beta'
       }
     }
   ]
