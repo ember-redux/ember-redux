aa645e901cd32f26bf364d325f9a9341a81f7060
Ken Snyder <ken@ken.net>
2016-09-14 19:46:06 +0100

    more descriptive reducter template

--- blueprints/reducer/files/__root__/reducers/__name__.js
+++ blueprints/reducer/files/__root__/reducers/__name__.js
@@ -1,13 +1,33 @@
 // __name__ REDUCER
+import Ember from 'ember';
 
-var reducer = (state, action) => {
+/**
+ * This reducer takes in the localised state which
+ * is owned by this reducer; it's job is create a
+ * new (non0-mutated) state object for this branch of
+ * the overall state tree.
+ *
+ * Note: as best practice you should always assign
+ * a default value for state and handle all unknown
+ * actions by returning the state back unchanged.
+ */
+var reducer = (state = {}, action) => {
 
   switch(action.type) {
 
-    case 'ADD':
-      return state + 1;
-    case 'SUBTRACT':
-      return state - 1;
+    case 'FOO':
+      return Ember.assign({}, state, {
+        uno: 1,
+        dos: 2,
+        tres: 3
+      });
+
+    case 'BAR':
+      return Ember.assign({}, state, {
+        uno: 0,
+        dos: 0,
+        tres: 0
+      });
 
     default:
       return state;
