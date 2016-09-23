cc91a9114d3b9d74f8ec72defc70d5fc64119945
toran billups <toranb@gmail.com>
2016-09-23 05:58:36 -0500

    [BUILD]: updated ember-try config to run 2.0/LTS/Beta releases

--- config/ember-try.js
+++ config/ember-try.js
@@ -11,33 +11,6 @@ module.exports = {
       }
     },
     {
-      name: 'ember-2.1.X',
-      dependencies: {
-        'ember': '~2.1.0'
-      },
-      resolutions: {
-        'ember': '~2.1.0'
-      }
-    },
-    {
-      name: 'ember-2.2.X',
-      dependencies: {
-        'ember': '~2.2.0'
-      },
-      resolutions: {
-        'ember': '~2.2.0'
-      }
-    },
-    {
-      name: 'ember-2.3.X',
-      dependencies: {
-        'ember': '~2.3.0'
-      },
-      resolutions: {
-        'ember': '~2.3.0'
-      }
-    },
-    {
       name: 'ember-2.4.X',
       dependencies: {
         'ember': '~2.4.0'
@@ -47,30 +20,12 @@ module.exports = {
       }
     },
     {
-      name: 'ember-2.5.X',
+      name: 'ember-2.8.X',
       dependencies: {
-        'ember': '~2.5.0'
+        'ember': '~2.8.0'
       },
       resolutions: {
-        'ember': '~2.5.0'
-      }
-    },
-    {
-      name: 'ember-2.6.X',
-      dependencies: {
-        'ember': '~2.6.0'
-      },
-      resolutions: {
-        'ember': '~2.6.0'
-      }
-    },
-    {
-      name: 'ember-2.7.X',
-      dependencies: {
-        'ember': '~2.7.0'
-      },
-      resolutions: {
-        'ember': '~2.7.0'
+        'ember': '~2.8.0'
       }
     },
     {
