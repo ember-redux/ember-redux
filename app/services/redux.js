545714173ed3fdf8ebdecbdaedd1afefda36160f
Chris Freeman <cfreeman.code@gmail.com>
2016-10-05 12:39:06 -0500

    Add ability to pass setup config for middleware
    When configuring middleware, users can now optionally export a hash containing:
    - middleware: an array of middleware
    - setup: a thunk containing setup instructions to be run after the store is created.

--- app/services/redux.js
+++ app/services/redux.js
@@ -3,7 +3,18 @@ import redux from 'npm:redux';
 import reducers from '../reducers/index';
 import enhancers from '../enhancers/index';
 import optional from '../reducers/optional';
-import middleware from '../middleware/index';
+import middlewareConfig from '../middleware/index';
+
+// Util for handling the case where no setup thunk was created in middleware
+const noOp = () => {};
+
+// Handle "classic" middleware exports (i.e. an array), as well as the hash option
+const extractMiddlewareConfig = (mc) => {
+  return Ember.isArray(mc) ? { middleware: mc } : mc;
+}
+
+// Destructure the middleware array and the setup thunk into two different variables
+const { middleware, setup = noOp } = extractMiddlewareConfig(middlewareConfig);
 
 var { createStore, applyMiddleware, combineReducers, compose } = redux;
 var createStoreWithMiddleware = compose(applyMiddleware(...middleware), enhancers)(createStore);
@@ -11,6 +22,7 @@ var createStoreWithMiddleware = compose(applyMiddleware(...middleware), enhancer
 export default Ember.Service.extend({
     init() {
         this.store = createStoreWithMiddleware(optional(combineReducers(reducers)));
+        setup();
         this._super(...arguments);
     },
     getState() {
