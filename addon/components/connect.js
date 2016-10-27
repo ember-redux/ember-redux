b5b0584272622f3dd8babaa1efab192e867315cd
Brett Burley <brett@adakist.com>
2016-10-27 02:37:15 -0700

    Cache the mapped state in components.
    This prevents the mapStateToComputed function from being called for
    every single property that it provides to the component. Additionally,
    this change also only notifies for properties that have actually
    changed their value to reduce the amount of rendering required when
    there are changes.

--- addon/components/connect.js
+++ addon/components/connect.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
-import shallowEqual from '../-private/equal';
 
 const { computed, defineProperty } = Ember;
 
@@ -8,14 +7,7 @@ var connect = function(mapStateToComputed, mapDispatchToActions) {
     var finalMapStateToComputed = mapStateToComputed || function() {return {};};
     var finalMapDispatchToActions = mapDispatchToActions || function() {return {};};
     return function wrapWithConnect(WrappedComponent) {
-        var mapState = function(state) {
-            var props = [];
-            Object.keys(finalMapStateToComputed(state)).forEach(function(key) {
-                props.push(key);
-            });
-            return props;
-        };
-        var mapDispatch= function(dispatch) {
+        var mapDispatch = function(dispatch) {
             var actions = [];
             Object.keys(finalMapDispatchToActions(dispatch)).forEach(function(key) {
                 actions.push(key);
@@ -24,46 +16,52 @@ var connect = function(mapStateToComputed, mapDispatchToActions) {
         };
         return WrappedComponent.extend({
             redux: Ember.inject.service('redux'),
+
             init() {
                 var component = this;
-                component['actions'] = Ember.$.extend({}, component['actions']);
                 var redux = this.get('redux');
-                var props = mapState(redux.getState());
+
+                // Set up actions.
                 var dispatch = mapDispatch(redux.dispatch.bind(redux));
-                props.forEach(function(name) {
-                    defineProperty(component, name, computed(function() {
-                        return finalMapStateToComputed(redux.getState())[name];
-                    }).property().readOnly());
-                });
+                component['actions'] = Ember.$.extend({}, component['actions']);
                 dispatch.forEach(function(action) {
                     component['actions'][action] = finalMapDispatchToActions(redux.dispatch.bind(redux))[action];
                 });
+
+                // Set up properties.
+                const state = finalMapStateToComputed(redux.getState());
+                this._localState = state;
+                Object.keys(state).forEach(key => {
+                    defineProperty(component, key, computed(function() {
+                        return this._localState[key];
+                    }).readOnly());
+                });
+
                 if (shouldSubscribe && !this.unsubscribe) {
-                    this.unsubscribe = redux.subscribe(this.handleChange.bind(this));
+                    this.unsubscribe = redux.subscribe(() => this.handleChange());
                 }
+
                 this._super(...arguments);
             },
+
             handleChange() {
-                var redux = this.get('redux');
-                var props = mapState(redux.getState());
-                var componentState = this.getComponentState(props);
-                var reduxState = finalMapStateToComputed(redux.getState());
-                if (!shallowEqual(componentState, reduxState)) {
-                    this.updateProps(props);
-                }
-            },
-            getComponentState(props) {
-                var componentState = {};
-                props.forEach((name) => {
-                    componentState[name] = this.get(name);
-                });
-                return componentState;
-            },
-            updateProps(props) {
-                props.forEach((name) => {
-                    this.notifyPropertyChange(name);
+                const redux = this.get('redux');
+                const oldState = this._localState;
+                const newState = finalMapStateToComputed(redux.getState());
+
+                if (oldState === newState) { return; }
+
+                this._localState = newState;
+
+                // Mark changed properties.
+                Object.keys(newState).forEach(key => {
+                    const cachedValue = this.cacheFor(key);
+                    if (cachedValue !== undefined && cachedValue !== newState[key]) {
+                        this.notifyPropertyChange(key);
+                    }
                 });
             },
+
             willDestroy() {
                 if (this.unsubscribe) {
                     this.unsubscribe();
