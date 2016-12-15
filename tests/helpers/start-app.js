1d95538380711dab221c25ec7629b51c5367ba1f
toran billups <toranb@gmail.com>
2016-12-14 20:20:01 -0600

    [BUILD]: added ember 2.10 to ember-try/travis

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -28,7 +28,7 @@ export default function startApp(attrs) {
   let application;
 
   // use defaults, but you can override
-  let attributes = Ember.assign({}, config.APP, attrs);
+  let attributes = Object.assign({}, config.APP, attrs);
 
   Ember.run(() => {
     application = Application.create(attributes);
