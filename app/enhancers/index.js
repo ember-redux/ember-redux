26299736382f94a25b9399ffd5e1015ed24218a6
Matthew Dahl <mdahl@ciena.com>
2017-03-03 21:49:04 -0800

    Address PR concerns

--- app/enhancers/index.js
+++ app/enhancers/index.js
@@ -1,9 +1,1 @@
-export default function(...funcs) {
-  return (...args) => {
-    if (funcs.length === 0) {
-      return args[0];
-    }
-    var last = funcs[funcs.length - 1];
-    return last(...args);
-  };
-}
+export {default} from 'ember-redux/enhancers'
