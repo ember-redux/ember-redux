8340491b43dd799ee0a7c92ee514b63a35048b21
toran billups <toranb@gmail.com>
2017-04-11 22:40:53 -0500

    [BUILD]: drop ember 2.0/2.4 from the travis config

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,24 +1,6 @@
 module.exports = {
   scenarios: [
     {
-      name: 'ember-2.0.X',
-      dependencies: {
-        'ember': '~2.0.0'
-      },
-      resolutions: {
-        'ember': '~2.0.0'
-      }
-    },
-    {
-      name: 'ember-2.4.X',
-      dependencies: {
-        'ember': '~2.4.0'
-      },
-      resolutions: {
-        'ember': '~2.4.0'
-      }
-    },
-    {
       name: 'ember-2.8.X',
       dependencies: {
         'ember': '~2.8.0'
