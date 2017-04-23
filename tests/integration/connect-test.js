f58b21f7f9e621ccd21a1e48949641c686f2cab1
Brett Burley <brett@adakist.com>
2017-04-22 21:25:31 -0700

    Add test to verify stateToComputed call count.

--- tests/integration/connect-test.js
+++ tests/integration/connect-test.js
@@ -3,6 +3,8 @@ import connect from 'ember-redux/components/connect';
 import hbs from 'htmlbars-inline-precompile';
 import { moduleForComponent, test } from 'ember-qunit';
 
+const { Component } = Ember;
+
 moduleForComponent('count-list', 'integration: connect test', {
   integration: true,
   setup() {
@@ -71,6 +73,29 @@ test('stateToComputed can be used with component level CP if notifyPropertyChang
   assert.equal(this.$('.dyno').text(), 'name: Tom', 'should render new value when local component CP changed and notifyPropertyChange invoked');
 });
 
+test('stateToComputed is not invoked extraneously', function(assert) {
+  let callCount = 0;
+  const stateToComputed = () => {
+    callCount++;
+    return { callCount };
+  }
+  this.register('component:test-component', connect(stateToComputed)(Component.extend({
+    layout: hbs`{{callCount}}`
+  })));
+
+  this.render(hbs`{{test-component attr=attr}}`);
+  assert.equal(this.$().text(), '1');
+  assert.equal(callCount, 1);
+
+  this.set('attr', 'some-change');
+  assert.equal(this.$().text(), '2');
+  assert.equal(callCount, 2);
+
+  this.get('redux').dispatch({ type: 'FAKE-ACTION' });
+  assert.equal(this.$().text(), '3');
+  assert.equal(callCount, 3);
+});
+
 test('the component should truly be extended meaning actions map over as you would expect', function(assert) {
   this.render(hbs`{{count-list}}`);
   let $random = this.$('.random-state');
@@ -95,7 +120,7 @@ test('each computed is truly readonly', function(assert) {
 
 test('lifecycle hooks are still invoked', function(assert) {
   assert.expect(3);
-  this.register('component:test-component', connect()(Ember.Component.extend({
+  this.register('component:test-component', connect()(Component.extend({
     init() {
       assert.ok(true, 'init is invoked');
       this._super(...arguments);
@@ -121,14 +146,14 @@ test('connecting dispatchToActions only', function(assert) {
   assert.expect(2);
   const dispatchToActions = () => {};
 
-  this.register('component:test-component-1', connect(null, dispatchToActions)(Ember.Component.extend({
+  this.register('component:test-component-1', connect(null, dispatchToActions)(Component.extend({
     init() {
       this._super(...arguments);
       assert.ok(true, 'should be able to connect components passing `null` to stateToComputed');
     }
   })));
 
-  this.register('component:test-component-2', connect(undefined, dispatchToActions)(Ember.Component.extend({
+  this.register('component:test-component-2', connect(undefined, dispatchToActions)(Component.extend({
     init() {
       this._super(...arguments);
       assert.ok(true, 'should be able to connect components passing `undefined` to stateToComputed');
@@ -157,7 +182,7 @@ test('connecting dispatchToActions as object should dispatch action', function(a
     }
   };
 
-  this.register('component:test-dispatch-action-object', connect(undefined, dispatchToActions)(Ember.Component.extend({
+  this.register('component:test-dispatch-action-object', connect(undefined, dispatchToActions)(Component.extend({
     init() {
       this._super(...arguments);
     },
@@ -182,7 +207,7 @@ test('calling handleChange directly shows a deprecation warning', function(asser
     assert.equal(id, 'ember-redux.no-public-handle-change');
   }
 
-  this.register('component:test-component', connect()(Ember.Component.extend({
+  this.register('component:test-component', connect()(Component.extend({
     init() {
       this.handleChange();
       this._super(...arguments);
