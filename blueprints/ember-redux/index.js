74f65e31fdcca977e01ccf48ff2a16c5449eef57
toran billups <toranb@gmail.com>
2017-08-20 09:37:50 -0500

    [REDUX]: bump each of the shims again...

--- blueprints/ember-redux/index.js
+++ blueprints/ember-redux/index.js
@@ -5,8 +5,8 @@ module.exports = {
   afterInstall: function() {
     return this.addAddonsToProject({
       packages: [
-        {name: 'ember-redux-shim', target: '^2.1.0'},
-        {name: 'ember-redux-thunk-shim', target: '^2.0.0'}
+        {name: 'ember-redux-shim', target: '^2.4.0'},
+        {name: 'ember-redux-thunk-shim', target: '^2.3.0'}
       ]
     });
   }
