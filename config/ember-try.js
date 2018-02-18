8fce5ef6b7bcf5f2ac9f16fb99a0f7ba7e6907ad
toran billups <toranb@gmail.com>
2018-02-18 14:51:05 -0600

    DEPENDENCY: upgrade to ember-cli & ember v3.0

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,18 +3,18 @@ module.exports = {
   useYarn: true,
   scenarios: [
     {
-      name: 'ember-2.16.X',
-      npm: {
-        devDependencies: {
-          'ember-source': '~2.16.0'
-        }
-      }
-    },
-    {
       name: 'ember-2.18.X',
       npm: {
         devDependencies: {
           'ember-source': '~2.18.0'
+        }
+      }
+    },
+    {
+      name: 'ember-3.0.X',
+      npm: {
+        devDependencies: {
+          'ember-source': '~3.0.0'
         }
       }
     },
