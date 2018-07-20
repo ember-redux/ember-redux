8794c75c3d461e0c1409e183c250bd86646a04e0
toran billups <toranb@gmail.com>
2018-07-20 16:52:39 -0500

    DEPENDENCY: updated ember/ember-cli to v3.3.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
         'index.js',
         'testem.js',
         'ember-cli-build.js',
+        'blueprints/*/index.js',
         'config/**/*.js',
         'tests/dummy/config/**/*.js'
       ],
