a08873b06b1a019bdef9f58c583b043a3f80a43d
jkusa <jkilroy@gmail.com>
2019-08-11 13:39:46 -0500

    Use ember-auto-import in lieu of a redux shim

--- blueprints/ember-redux/index.js
+++ blueprints/ember-redux/index.js
@@ -1,11 +1,10 @@
 module.exports = {
   description: 'Installation blueprint for ember-redux',
-  normalizeEntityName: function() {},
+  normalizeEntityName() {},
 
-  afterInstall: function() {
+  afterInstall() {
     return this.addAddonsToProject({
       packages: [
-        {name: 'ember-redux-shim', target: '^4.0.1'},
         {name: 'ember-redux-thunk-shim', target: '^2.5.0'}
       ]
     });
