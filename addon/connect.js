4d1026f6f71f53d9847e67b95777a5727ffbe686
toran billups <toranb@gmail.com>
2019-12-11 05:42:20 -0600

    REMOVE: dropped support for ember < 3.6

--- addon/connect.js
+++ addon/connect.js
@@ -1,14 +1,9 @@
 import Component from '@ember/component';
-import wrapEmberObject from './ember-object';
 import wrapEs2015Class from './es2015-class';
 
 export default (stateToComputed, dispatchToActions=() => ({})) => {
   return IncomingComponent => {
     const WrappedComponent = IncomingComponent || Component;
-
-    if(WrappedComponent.hasOwnProperty('extend')) {
-      return wrapEmberObject(stateToComputed, dispatchToActions, WrappedComponent);
-    }
 
     return wrapEs2015Class(stateToComputed, dispatchToActions, WrappedComponent);
   };
