4f720e892c79c1725b4c88c06cb2d034dbc3ac9e
Raghavendra Bhat <raghavendra.bhat2@rsa.com>
2020-02-02 20:30:35 -0800

    Adding support for octane component

--- addon/connect.js
+++ addon/connect.js
@@ -1,10 +1,17 @@
 import Component from '@ember/component';
 import wrapEs2015Class from './es2015-class';
+import octaneConnect from './octane-connect'
 
+function isClassicComponent(component) {
+  // Not finding any other better way check component is classic or octane
+  return component.toString() === '@ember/component' && component.reopenClass;
+}
 export default (stateToComputed, dispatchToActions=() => ({})) => {
   return IncomingComponent => {
     const WrappedComponent = IncomingComponent || Component;
-
-    return wrapEs2015Class(stateToComputed, dispatchToActions, WrappedComponent);
+    if (isClassicComponent(WrappedComponent)) {
+      return wrapEs2015Class(stateToComputed, dispatchToActions, WrappedComponent);
+    }
+    return octaneConnect(stateToComputed, dispatchToActions, WrappedComponent);
   };
 };
