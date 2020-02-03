776c8a1a5d1987dc3e1e5deaf9148f8ed6060cc2
Raghavendra Bhat <raghavendra.bhat2@rsa.com>
2020-02-02 21:07:58 -0800

    Updated the ember-try version

--- config/ember-try.js
+++ config/ember-try.js
@@ -6,26 +6,10 @@ module.exports = {
 
   scenarios: [
     {
-      name: 'ember-3.6.X',
+      name: 'ember-3.15.X',
       npm: {
         devDependencies: {
-          'ember-source': '~3.6.0'
-        }
-      }
-    },
-    {
-      name: 'ember-3.8.X',
-      npm: {
-        devDependencies: {
-          'ember-source': '~3.8.0'
-        }
-      }
-    },
-    {
-      name: 'ember-3.12.X',
-      npm: {
-        devDependencies: {
-          'ember-source': '~3.12.0'
+          'ember-source': '~3.15.0'
         }
       }
     }
