0849d18efb6a2426a6c50c7257fc1b4a8699d8b7
Frank Koenders <frank.koenders@gynzy.com>
2022-12-12 10:30:20 +0100

    Fix implicit this deprecation

--- tests/dummy/app/components/count-detail/component.js
+++ tests/dummy/app/components/count-detail/component.js
@@ -3,7 +3,7 @@ import hbs from 'htmlbars-inline-precompile';
 
 export default Component.extend({
   layout: hbs`
-    <span class="child-state">{{high}}</span>
-    <button class="btn-down" onclick={{down}}>down</button>
+    <span class="child-state">{{this.high}}</span>
+    <button class="btn-down" onclick={{this.down}}>down</button>
   `
 });
